52a38b0ceb570ab10f6ad64aad458e47da07c49e
Ous Bayaa <bayaaous@gmail.com>
2020-04-29 22:38:54 -0700

    created strains table

---
+++ database/migrations/20200429221601_saved_strains.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('strains', tbl => {
+        tbl.string('strain').notNullable().primary();
+        tbl.string('type').notNullable()
+        tbl.float('rating', 5).notNullable();
+        tbl.string('effects').notNullable();
+        tbl.string('flavour').notNullable();
+        tbl.text('description', 2000).notNullable();
+      })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('strains')
+};
