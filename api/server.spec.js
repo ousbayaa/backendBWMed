e47e6227c3f7d8e0fafee860b505475238b8e332
Ous Bayaa <bayaaous@gmail.com>
2020-05-01 08:50:14 -0700

    testing completed

--- api/server.spec.js
+++ api/server.spec.js
@@ -15,4 +15,31 @@ describe('TESTS', () => {
             .then(res => expect(res.status).toBe(201))
         })
     })
+    describe('Strains', () => {
+        it('should receive a 400 if a user does not provide a JWT', () => {
+            return request(server)
+            .post('/api/strains')
+            .then(res => expect(res.status).toBe(400))
+        })
+    })
+    describe('Login', () => {
+        it('should receive 401 if a user inputs incorrect password', () => {
+            return request(server)
+            .post('/api/auth/register')
+            .send({username:"mary", password:"jane"})
+            .then(res => {
+                return request(server)
+                .post('/api/auth/login')
+                .send({username:"mary", password:"test"})
+                .then(res => expect(res.status).toBe(401))
+            })
+        })
+    })
+    describe('Strain ID', () => {
+        it('should receive a 404 if a user inputs invalid Strain ID', () => {
+            return request(server)
+            .post('/api/strains/id')
+            
+        })
+    })
 });
